70da9e40acf61d90624e8d1e38e5702b670253f8
Blubfish <giabaota2020@gmail.com>
2026-02-07 10:12:27 -0600

    initial commit

--- commands/utility/math.js
+++ commands/utility/math.js
@@ -1,13 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const  getRandomMathQuestion  = require('../../questions/math_questions');
 
-/*
-The data property, which will provide the command definition shown above for registering to Discord.
-The execute method, which will contain the functionality to run from our event handler when the command is used.
-
-These are placed inside module.exports so they can be read by other files; namely the command loader and command deployment scripts mentioned earlier.
-*/
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('math')
