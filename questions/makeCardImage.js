87132320e9f1a40c3d62b6fe750df54c105375c4
Gia Bao Ta <giabaota2020@gmail.com>
2026-04-25 19:34:05 -0500

    Let puppeteer handle Chronium instead of hardcoding  path

--- questions/makeCardImage.js
+++ questions/makeCardImage.js
@@ -52,7 +52,7 @@ async function makeCardImage(currentCard) {
   `;
 
   const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/chromium-browser',
+    executablePath: puppeteer.executablePath(),
     args: [
       '--no-sandbox', 
       '--disable-setuid-sandbox',
